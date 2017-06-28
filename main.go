444f51ed65e42bf59dd9fdb1dec69ed91d547426
Robert Vollmert <rob@vllmrt.net>
2017-06-28 13:27:42 +0200

    /work

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/echo", EchoHandler)
+	http.HandleFunc("/work", WorkHandler)
 	log.Fatal(http.ListenAndServe(":8765", nil))
 }
 
@@ -15,4 +17,13 @@ func EchoHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "text/plain")
 	io.Copy(w, r.Body)
+}
+
+func work() {
+	time.Sleep(50*time.Millisecond)
+}
+
+func WorkHandler(w http.ResponseWriter, r *http.Request) {
+	work()
+	EchoHandler(w, r)
 }
